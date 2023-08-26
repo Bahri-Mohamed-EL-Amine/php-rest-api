e660952d3ae6b5e80b340abb72649726ae647c6b
Bahri-Mohamed-EL-Amine <bahrimohamedelamine22esi@gmail.com>
2023-08-26 14:15:58 +0100

    database connection set up

---
+++ config/database.php
@@ -0,0 +1,45 @@
+<?php
+class Database
+{
+
+
+    // declaration
+    private $hostname;
+    private $dbname;
+    private $username;
+    private $password;
+
+    private $conn;
+
+    public function connect()
+    {
+
+        // initialisation
+        $this->hostname = "localhost";
+        $this->dbname = "rest_api";
+        $this->username = "root";
+        $this->password = "admin";
+
+
+        // connection
+        $this->conn = new mysqli(
+            $this->hostname,
+            $this->username,
+            $this->password,
+            $this->dbname,
+        );
+
+        if ($this->conn->connect_errno) {
+            // has errors
+            print_r($this->conn->connect_error);
+            exit;
+        } else {
+            // no errors => return connection
+            echo "connected to database successfully";
+            return $this->conn;
+        }
+
+    }
+}
+
+?>
